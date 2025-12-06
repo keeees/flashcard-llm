dd730aa3d6484010beca9399a801a4cc8cd17653
Ke <Ke@DESKTOP-SEBFSOU>
2025-12-07 01:26:53 +0800

    fix netlify

--- frontend/src/components/TextInput.tsx
+++ frontend/src/components/TextInput.tsx
@@ -1,5 +1,4 @@
-import React, { useState, useEffect, useCallback } from 'react';
-import classNames from 'classnames';
+import React, { useState, useEffect } from 'react';
 
 interface TextInputProps {
   value: string;
